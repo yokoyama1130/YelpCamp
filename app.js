ce156a6c4ebe5d451177778b9f6fd65155eec57a
yokoyama1130 <ryogotan@ezweb.ne.jp>
2026-09-20 17:32:55 +0900

    モデルとスキーマの作成

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require("express");
-const app = express();
+const mongoose = require('mongoose');
 const path = require("path");
+const Campground = require("./models/campground");
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+    .then(() => {
+        console.log("コネクションOK");
+    })
+    .catch((err) => {
+        console.error("コネクションエラー");
+        console.error(err);
+    });
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({
+        title: "私",
+        description: "気軽"
+    });
+    await camp.save();
+    res.send(camp);
 });
 
 app.listen(3000, () => {
